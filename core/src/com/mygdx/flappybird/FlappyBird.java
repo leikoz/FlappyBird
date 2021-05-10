71e1dff88045aa7caa9e4ab2d51f1ebbdcf12816
Samuel <samuel25804@gmail.com>
2021-05-10 11:25:56 -0500

    Se añadió la clase MenuState

--- core/src/com/mygdx/flappybird/FlappyBird.java
+++ core/src/com/mygdx/flappybird/FlappyBird.java
@@ -1,9 +1,14 @@
 package com.mygdx.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+
+import States.GameStateManager;
+import States.MenuState;
 
 public class FlappyBird extends ApplicationAdapter {
 
@@ -11,26 +16,26 @@ public class FlappyBird extends ApplicationAdapter {
 	public static final int HEIGHT = 720;
 	public static final String TITLE = "FlappyBird";
 
-	SpriteBatch batch;
-	Texture img;
-	
+	private GameStateManager gsm;
+	private SpriteBatch batch;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gsm = new GameStateManager();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsm.push(new MenuState(gsm));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
